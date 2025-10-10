6fdba0dd2ea1e5e5882c3f237360ebe23be89bc4
Claudio Martinez <clamart85hs@gmail.com>
2025-10-10 18:23:51 +0200

    Replace bcrypt with bcryptjs for Vercel compatibility

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,5 @@
-
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || '';
